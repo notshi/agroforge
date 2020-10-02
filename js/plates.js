03f432b7dc0e18eea6b983b549227d63e44b47a3
kriss <kriss@xixs.com>
2020-10-02 19:48:54 +0100

    add plated plates

---
+++ js/plates.js
@@ -0,0 +1,14 @@
+
+let plates=exports
+
+plates.plated=require("plated").create({},{pfs:{}}) // create a base instance for inline chunks with no file access
+
+plates.chunks={}
+plates.replace=function(str){ return plates.plated.chunks.replace(str,plates.chunks) }
+
+plates.plated.chunks.fill_chunks( require('fs').readFileSync(__dirname + '/plates.html', 'utf8'), plates.chunks )
+plates.plated.chunks.fill_chunks( require('fs').readFileSync(__dirname + '/plates.css',  'utf8'), plates.chunks )
+plates.plated.chunks.fill_chunks( require('fs').readFileSync(__dirname + '/plates.svg',  'utf8'), plates.chunks )
+
+plates.plated.chunks.format_chunks( plates.chunks )
+
